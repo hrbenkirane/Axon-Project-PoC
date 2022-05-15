9267d68077407722e69eb09ae646e434f1d40a7c
Rachid Benkirane <rachid.benkirane@gmail.com>
2022-05-15 09:30:11 +0200

    Add JPA dependeny to products microservice and implementing the entity and repo for products

---
+++ ProductService/src/main/java/com/cwt/productservice/core/data/ProductsRepository.java
@@ -0,0 +1,9 @@
+package com.cwt.productservice.core.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductsRepository extends JpaRepository<ProductEntity, String> {
+    ProductEntity findByProductId(String productId);
+
+    ProductEntity findByProductIdOrTitle(String productId, String titile);
+}
