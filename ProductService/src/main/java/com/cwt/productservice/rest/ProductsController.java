12754ca43c029441099ea9a38fb92be936ac0b5e
Rachid Benkirane <rachid.benkirane@gmail.com>
2022-05-15 08:59:15 +0200

    Inject CommandGateway from axon to our controller and create createProduct Command

--- ProductService/src/main/java/com/cwt/productservice/rest/ProductsController.java
+++ ProductService/src/main/java/com/cwt/productservice/rest/ProductsController.java
@@ -1,19 +1,46 @@
 package com.cwt.productservice.rest;
 
+import com.cwt.productservice.command.CreateProductCommand;
+import com.netflix.discovery.converters.Auto;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
 
+    private final Environment environment;
+    private final CommandGateway commandGateway;
+
+    @Autowired
+    public ProductsController(Environment environment, CommandGateway commandGateway) {
+        this.environment = environment;
+        this.commandGateway = commandGateway;
+    }
+
     @PostMapping
-    public String createProduct() {
-        return "http post handled";
+    public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
+        CreateProductCommand createProductCommand = CreateProductCommand.builder()
+                .price(createProductRestModel.getPrice())
+                .quantity(createProductRestModel.getQuantity())
+                .title(createProductRestModel.getTitle())
+                .productId(UUID.randomUUID().toString()).build();
+        String returnValue;
+        try {
+            returnValue = commandGateway.sendAndWait(createProductCommand);
+        } catch (Exception ex) {
+            returnValue = ex.getLocalizedMessage();
+        }
+        return returnValue;
     }
 
     @GetMapping
     public String getProduct() {
-        return "http get handeled";
+        return "http get handeled " + environment.getProperty("local.server.port");
     }
 
     @PutMapping
